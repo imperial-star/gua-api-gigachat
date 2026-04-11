19446e224b80981bfd956bfcbbd579c40f6431e0
oxxx1mif <pluginexteragram@gmail.com>
2026-04-11 14:50:27 +1000

    update

--- app/src/main/java/api/ggroup/sber/MainActivity.kt
+++ app/src/main/java/api/ggroup/sber/MainActivity.kt
@@ -13,19 +13,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import api.ggroup.sber.ui.theme.GigachatapiTheme
 
 import gr3mp.giga.sber.auth.AccessToken
-
 import api.ggroup.sber.BuildVars.client_id
 import api.ggroup.sber.BuildVars.client_secret
+
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import java.io.IOException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,9 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GigachatapiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    GigaChatScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -44,11 +45,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    /*
-    Text(
-        text = "Hello $name!",
+fun GigaChatScreen(modifier: Modifier = Modifier) {
+    var resultText by remember { mutableStateOf("testToken") }
+    var isLoading by remember { mutableStateOf(false) }
+
+    Column(
         modifier = modifier
-    )
-     */
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = resultText,
+            style = MaterialTheme.typography.bodyMedium
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {
+                isLoading = true
+                resultText = "..."
+
+                AccessToken.getAccessToken(
+                    client_id,
+                    client_secret,
+                    "GIGACHAT_API_PERS",
+                    object : Callback {
+                        override fun onFailure(call: Call, e: IOException) {
+                            resultText = "error net: ${e.message}"
+                            isLoading = false
+                        }
+
+                        override fun onResponse(call: Call, response: Response) {
+                            val body = response.body?.string() ?: "-"
+
+                            resultText = if (response.isSuccessful) {
+                                "Token :\n$body"
+                            } else {
+                                "Error server (${response.code}): $body"
+                            }
+                            isLoading = false
+                        }
+                    }
+                )
+            },
+            enabled = !isLoading
+        ) {
+            Text(if (isLoading) "..." else "Token")
+        }
+    }
 }
--- g-api-sber/src/main/java/gr3mp/giga/sber/client/NetworkService.java
+++ g-api-sber/src/main/java/gr3mp/giga/sber/client/NetworkService.java
@@ -9,8 +9,15 @@
 
 package gr3mp.giga.sber.client;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
 import java.util.concurrent.TimeUnit;
-
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -19,10 +26,44 @@ public class NetworkService {
     private static NetworkService instance;
     private final OkHttpClient client;
 
+    // russian_trusted_root_ca.cer
+    private static final String PEM_CERTIFICATE =
+            "-----BEGIN CERTIFICATE-----\n" +
+                    "MIIFwjCCA6qgAwIBAgICEAAwDQYJKoZIhvcNAQELBQAwcDELMAkGA1UEBhMCUlUx\n" +
+                    "PzA9BgNVBAoMNlRoZSBNaW5pc3RyeSBvZiBEaWdpdGFsIERldmVsb3BtZW50IGFu\n" +
+                    "ZCBDb21tdW5pY2F0aW9uczEgMB4GA1UEAwwXUnVzc2lhbiBUcnVzdGVkIFJvb3Qg\n" +
+                    "Q0EwHhcNMjIwMzAxMjEwNDE1WhcNMzIwMjI3MjEwNDE1WjBwMQswCQYDVQQGEwJS\n" +
+                    "VTE/MD0GA1UECgw2VGhlIE1pbmlzdHJ5IG9mIERpZ2l0YWwgRGV2ZWxvcG1lbnQg\n" +
+                    "YW5kIENvbW11bmljYXRpb25zMSAwHgYDVQQDDBdSdXNzaWFuIFRydXN0ZWQgUm9v\n" +
+                    "dCBDQTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAMfFOZ8pUAL3+r2n\n" +
+                    "qqE0Zp52selXsKGFYoG0GM5bwz1bSFtCt+AZQMhkWQheI3poZAToYJu69pHLKS6Q\n" +
+                    "XBiwBC1cvzYmUYKMYZC7jE5YhEU2bSL0mX7NaMxMDmH2/NwuOVRj8OImVa5s1F4U\n" +
+                    "zn4Kv3PFlDBjjSjXKVY9kmjUBsXQrIHeaqmUIsPIlNWUnimXS0I0abExqkbdrXbX\n" +
+                    "YwCOXhOO2pDUx3ckmJlCMUGacUTnylyQW2VsJIyIGA8V0xzdaeUXg0VZ6ZmNUr5Y\n" +
+                    "Ber/EAOLPb8NYpsAhJe2mXjMB/J9HNsoFMBFJ0lLOT/+dQvjbdRZoOT8eqJpWnVD\n" +
+                    "U+QL/qEZnz57N88OWM3rabJkRNdU/Z7x5SFIM9FrqtN8xewsiBWBI0K6XFuOBOTD\n" +
+                    "4V08o4TzJ8+Ccq5XlCUW2L48pZNCYuBDfBh7FxkB7qDgGDiaftEkZZfApRg2E+M9\n" +
+                    "G8wkNKTPLDc4wH0FDTijhgxR3Y4PiS1HL2Zhw7bD3CbslmEGgfnnZojNkJtcLeBH\n" +
+                    "BLa52/dSwNU4WWLubaYSiAmA9IUMX1/RpfpxOxd4Ykmhz97oFbUaDJFipIggx5sX\n" +
+                    "ePAlkTdWnv+RWBxlJwMQ25oEHmRguNYf4Zr/Rxr9cS93Y+mdXIZaBEE0KS2iLRqa\n" +
+                    "OiWBki9IMQU4phqPOBAaG7A+eP8PAgMBAAGjZjBkMB0GA1UdDgQWBBTh0YHlzlpf\n" +
+                    "BKrS6badZrHF+qwshzAfBgNVHSMEGDAWgBTh0YHlzlpfBKrS6badZrHF+qwshzAS\n" +
+                    "BgNVHRMBAf8ECDAGAQH/AgEEMA4GA1UdDwEB/wQEAwIBhjANBgkqhkiG9w0BAQsF\n" +
+                    "AAOCAgEAALIY1wkilt/urfEVM5vKzr6utOeDWCUczmWX/RX4ljpRdgF+5fAIS4vH\n" +
+                    "tmXkqpSCOVeWUrJV9QvZn6L227ZwuE15cWi8DCDal3Ue90WgAJJZMfTshN4OI8cq\n" +
+                    "W9E4EG9wglbEtMnObHlms8F3CHmrw3k6KmUkWGoa+/ENmcVl68u/cMRl1JbW2bM+\n" +
+                    "/3A+SAg2c6iPDlehczKx2oa95QW0SkPPWGuNA/CE8CpyANIhu9XFrj3RQ3EqeRcS\n" +
+                    "AQQod1RNuHpfETLU/A2gMmvn/w/sx7TB3W5BPs6rprOA37tutPq9u6FTZOcG1Oqj\n" +
+                    "C/B7yTqgI7rbyvox7DEXoX7rIiEqyNNUguTk/u3SZ4VXE2kmxdmSh3TQvybfbnXV\n" +
+                    "4JbCZVaqiZraqc7oZMnRoWrXRG3ztbnbes/9qhRGI7PqXqeKJBztxRTEVj8ONs1d\n" +
+                    "WN5szTwaPIvhkhO3CO5ErU2rVdUr89wKpNXbBODFKRtgxUT70YpmJ46VVaqdAhOZ\n" +
+                    "D9EUUn4YaeLaS8AjSF/h7UkjOibNc4qVDiPP+rkehFWM66PVnP1Msh93tc+taIfC\n" +
+                    "EYVMxjh8zNbFuoc7fzvvrFILLe7ifvEIUqSVIC/AzplM/Jxw7buXFeGP1qVCBEHq\n" +
+                    "391d/9RAfaZ12zkwFsl+IKwE/OZxW8AHa9i1p4GO0YSNuczzEm4=\n" +
+                    "-----END CERTIFICATE-----";
+
     private NetworkService() {
-        this.client = new OkHttpClient.Builder()
-                .connectTimeout(10, TimeUnit.SECONDS)
-                .build();
+        this.client = createInternalTrustedClient();
     }
 
     public static synchronized NetworkService getInstance() {
@@ -34,5 +75,36 @@ public class NetworkService {
 
     public void execute(Request request, Callback callback) {
         client.newCall(request).enqueue(callback);
+    }
+
+    private OkHttpClient createInternalTrustedClient() {
+        try {
+            InputStream is = new ByteArrayInputStream(PEM_CERTIFICATE.getBytes());
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            Certificate ca = cf.generateCertificate(is);
+            is.close();
+
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca_sber", ca);
+
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(
+                    TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(keyStore);
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, tmf.getTrustManagers(), null);
+
+            return new OkHttpClient.Builder()
+                    .connectTimeout(15, TimeUnit.SECONDS)
+                    .sslSocketFactory(sslContext.getSocketFactory(),
+                            (X509TrustManager) tmf.getTrustManagers()[0])
+                    .build();
+
+        } catch (Exception e) {
+            return new OkHttpClient.Builder()
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .build();
+        }
     }
 }
